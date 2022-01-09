cd3c1ec76dd832fd951e7d098bb53054eb33c73b
DanielNduati <dannduati2@gmail.com>
2022-01-09 13:50:29 +0300

    Update image capture test

--- test_image.py
+++ test_image.py
@@ -1,6 +1,7 @@
 import cv2
+from datetime import datetime
 cap = cv2.VideoCapture(0)
-ret,frame = cap.read()
+ret, frame = cap.read()
 if ret:
-    cv2.imwrite('images/image.jpg',frame)
+    cv2.imwrite(f'images/image_{datetime.now()}.jpg', frame)
 cap.release()
